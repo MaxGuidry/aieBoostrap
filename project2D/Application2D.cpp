b9dccbd076ed11d88fb10cb55588e4dedd855a03
MaxGuidry <mguidry7@outlook.com>
2016-12-08 14:16:50 -0600

    commiting just in case

--- project2D/Application2D.cpp
+++ project2D/Application2D.cpp
@@ -21,7 +21,7 @@ bool Application2D::startup()
 	m_shipTexture = new aie::Texture("./textures/ship.png");
 
 	m_font = new aie::Font("./font/consolas.ttf", 32);
-
+	
 	//m_audio = new aie::Audio("./audio/powerup.wav");
 	m_explosion = new aie::Audio("./audio/Explosion.wav");
 	m_shootSound = new aie::Audio("./audio/Laser.wav");
@@ -69,7 +69,13 @@ void Application2D::update(float deltaTime)
 	// input example
 	aie::Input* input = aie::Input::getInstance();
 
-
+	for (int i = 0; i < m_MaxBullets; i++)
+	{
+		if (Magazine[i].m_isShooting == false)
+		{
+			Magazine[i].position.x = Player1.m_playerPosition.x;
+		}
+	}
 	if (input->isKeyDown(aie::INPUT_KEY_RIGHT) && Player1.m_playerPosition.x < 1230)//change to variable storing window size
 	{
 		Player1.m_playerPosition = Player1.m_playerPosition + Vector2(7, 0);
@@ -78,10 +84,10 @@ void Application2D::update(float deltaTime)
 	{
 		Player1.m_playerPosition = Player1.m_playerPosition - Vector2(7, 0);
 	}
-	if (input->isKeyDown(aie::INPUT_KEY_SPACE)&&Player1.m_shotCooldown>=.2)
+	if (input->isKeyDown(aie::INPUT_KEY_SPACE)&&Player1.m_shotCooldown>=.16)
 	{
 		Magazine[numberOfBullets].position.x = Player1.m_playerPosition.x;
-		Player1.m_isShooting = true;
+   		Player1.m_isShooting = true;
 		Magazine[numberOfBullets].m_isShooting = true;
 		Magazine[numberOfBullets].m_shootTimer = 0;
 		(numberOfBullets < m_MaxBullets) ? numberOfBullets++ : numberOfBullets += 0;
@@ -112,7 +118,7 @@ void Application2D::update(float deltaTime)
 		
 		for (int j = 0; j < m_MaxBullets; j++)
 		{
-			for (int i = 0; i < m_MaxBullets; i++)
+			for (int i = 0; i < numberOfEnemies; i++)
 			{
 				if ((Enemies[i].m_position.x - Enemies[i].m_width < Magazine[j].position.x - 5 && Enemies[i].m_position.x + Enemies[i].m_width > Magazine[j].position.x - 5
 					|| Enemies[i].m_position.x - Enemies[i].m_width < Magazine[j].position.x + 5 && Enemies[i].m_position.x + Enemies[i].m_width > Magazine[j].position.x + 5)
@@ -127,6 +133,10 @@ void Application2D::update(float deltaTime)
 				
 			}
 		}
+	}
+	if (Player1.m_killCount == 20)
+	{
+		m_GameOver = true;
 	}
 	Player1.m_shotCooldown += deltaTime;
 	// example of audio
@@ -143,70 +153,80 @@ void Application2D::draw()
 
 	// wipe the screen to the background colour
 	clearScreen();
-
-	// set the camera position before we begin rendering
-	m_2dRenderer->setCameraPos(m_cameraX, m_cameraY);
-
-	// begin drawing sprites
-	m_2dRenderer->begin();
-
-
-	// demonstrate spinning sprite
-	m_2dRenderer->setUVRect(0, 0, 1, 1);
-	m_2dRenderer->drawSprite(m_shipTexture, Player1.m_playerPosition.x, 50, 0, 0, 0, 1);
-
-
-
-	// output some text, uses the last used colour
-	char fps[32];
-	sprintf_s(fps, 32, "FPS: %i", getFPS());
-	m_2dRenderer->drawText(m_font, fps, 0, 720 - 32);
-	char KillBuffer[32];
-	sprintf_s(KillBuffer, 32, "Kill Count: %i", Player1.m_killCount);
-	m_2dRenderer->drawText(m_font, KillBuffer, 0, 720 - 64);
-	for (int i = 0; i < m_MaxBullets; i++)
+	if (m_GameOver == false)
 	{
-		if (Player1.m_isShooting == true)
+		// set the camera position before we begin rendering
+		m_2dRenderer->setCameraPos(m_cameraX, m_cameraY);
+
+		// begin drawing sprites
+		m_2dRenderer->begin();
+
+
+		// demonstrate spinning sprite
+		m_2dRenderer->setUVRect(0, 0, 1, 1);
+		m_2dRenderer->drawSprite(m_shipTexture, Player1.m_playerPosition.x, 50, 0, 0, 0, 1);
+
+
+
+		// output some text, uses the last used colour
+		char fps[32];
+		sprintf_s(fps, 32, "FPS: %i", getFPS());
+		m_2dRenderer->drawText(m_font, fps, 0, 720 - 32);
+		char KillBuffer[32];
+		sprintf_s(KillBuffer, 32, "Kill Count: %i", Player1.m_killCount);
+		m_2dRenderer->drawText(m_font, KillBuffer, 0, 720 - 64);
+		for (int i = 0; i < m_MaxBullets; i++)
 		{
-			m_2dRenderer->setRenderColour(1, .5, .1, 1);
-			Magazine[i].position.y = 107 + Magazine[i].m_shootTimer;
-			m_2dRenderer->drawBox(Magazine[i].position.x, 107 + Magazine[i].m_shootTimer, 5, 30, 0, 0);
+			if (Player1.m_isShooting == true)
+			{
+				m_2dRenderer->setRenderColour(1, .5, .1, 1);
+				Magazine[i].position.y = 107 + Magazine[i].m_shootTimer;
+				m_2dRenderer->drawBox(Magazine[i].position.x, 107 + Magazine[i].m_shootTimer, 5, 30, 0, 0);
+			}
+		}
+
+		for (int i = 0; i < numberOfEnemies; i++)
+		{
+
+			m_2dRenderer->setRenderColour(.5, .5, .3, 1);
+			m_2dRenderer->drawBox(Enemies[i].m_position.x, 600, Enemies[i].m_height, Enemies[i].m_width, 0, 0);
+
+		}
+		if (m_backgroundColor1 > m_lastBackgroundColor1)
+		{
+			m_lastBackgroundColor1 = m_backgroundColor1;
+			m_backgroundColor1 += .01;
+			m_backgroundColor2 -= .01;
+			this->setBackgroundColour(m_backgroundColor1, m_backgroundColor2, -m_backgroundColor1, 1);
+		}
+		if (m_backgroundColor1 > 1)
+		{
+			m_backgroundColor1 -= .02;
+			m_backgroundColor2 += .02;
+		}
+		else if (m_backgroundColor1 < m_lastBackgroundColor1)
+		{
+			m_lastBackgroundColor1 = m_backgroundColor1;
+			m_backgroundColor2 += .01;
+			m_backgroundColor1 -= .01;
+
+			this->setBackgroundColour(m_backgroundColor1, m_backgroundColor2, -m_backgroundColor1, 1);
+		}
+		if (m_backgroundColor1 < -1)
+		{
+			m_backgroundColor1 += .02;
+			m_backgroundColor2 -= .02;
 		}
 	}
-
-	for (int i = 0; i < numberOfEnemies; i++)
+	else
 	{
-
-		m_2dRenderer->setRenderColour(.5, .5, .3, 1);
-		m_2dRenderer->drawBox(Enemies[i].m_position.x, 600, Enemies[i].m_height, Enemies[i].m_width, 0, 0);
+		m_font = new aie::Font("./font/consolas.ttf", 220);
+		this->setBackgroundColour(0, 0, 0, 1);
+		char GameOver[20];
+		sprintf_s(GameOver, 20, "Game Over!");
+		m_2dRenderer->drawText(m_font, GameOver, 45, 720 / 2, 0);
 
 	}
-	if (m_backgroundColor1 > m_lastBackgroundColor1)
-	{
-		m_lastBackgroundColor1 = m_backgroundColor1;
-		m_backgroundColor1 += .01;
-		m_backgroundColor2 -= .01;
-		this->setBackgroundColour(m_backgroundColor1, m_backgroundColor2, -m_backgroundColor1, 1);
-	}
-	if (m_backgroundColor1 > 1)
-	{
-		m_backgroundColor1 -= .02;
-		m_backgroundColor2 += .02;
-	}
-	else if (m_backgroundColor1 < m_lastBackgroundColor1)
-	{
-		m_lastBackgroundColor1 = m_backgroundColor1;
-		m_backgroundColor2 += .01;
-		m_backgroundColor1 -= .01;
-
-		this->setBackgroundColour(m_backgroundColor1, m_backgroundColor2, -m_backgroundColor1, 1);
-	}
-	if (m_backgroundColor1 < -1)
-	{
-		m_backgroundColor1 += .02;
-		m_backgroundColor2 -= .02;
-	}
-
 	// done drawing sprites
 	m_2dRenderer->end();
 }
